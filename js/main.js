c768f9a6a204014703e3acc937b96abc724ad1bc
Aleksej <silakovaleksej@icloud.com>
2023-10-16 18:47:39 +0300

    Добавляет функцию createImagesData (WIP)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const getRandomIntegers = (min, max) => {
   return Math.floor(randomNumber);
 };
 
-const getRandomItem = (items) => [getRandomIntegers(0, items.length - 1)];
+const getRandomItem = (items) => items[getRandomIntegers(0, items.length - 1)];
 
-void (getRandomIntegers(), getRandomItem());
+const createImagesData = (itemCount = 25) => {
+  const description = ['Описание 1', 'Описание 2', 'Описание 3'];
+  return new Array(25).fill(1).map((start,index) => ({
+    id: start + index,
+    url: `photos/${start + index}.jpg`,
+    description: getRandomItem(description),
+    likes: getRandomIntegers(15, 200),
+    Comment:[] // TODO: createCommentsData
+  }));
+};
+void (getRandomIntegers, getRandomItem, createImagesData);
