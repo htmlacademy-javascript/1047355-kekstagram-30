03de866514196acffbbcfe2ce4a4d18c1c28d216
Aleksej <silakovaleksej@icloud.com>
2023-10-30 16:47:30 +0300

    Добавляет рефакторинг добавления класса

--- js/gallery/comments.js
+++ js/gallery/comments.js
@@ -5,7 +5,7 @@ const createComment = (comments) => {
   const {avatar, name, message } = comments;
   const comment = document.createElement('li');
   comment.innerHTML = '<img class="social__picture" src="" alt="" width="35" height="35"><p class="social__text"></p>';
-  comment.classList.add('social__picture');
+  comment.classList.add('social__comment');
   comment.querySelector('.social__picture').src = avatar;
   comment.querySelector('.social__picture').alt = name;
   comment.querySelector('.social__text').textContent = message;
