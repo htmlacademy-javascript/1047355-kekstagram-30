ba05953fd725cbf512902d8b92ed857f30fd08c0
Aleksej <silakovaleksej@icloud.com>
2023-10-30 15:14:55 +0300

    Добавляет модуль создания комментариев

--- js/comments.js
+++ js/comments.js
@@ -1,19 +1,20 @@
 const container = document.querySelector('.social__comments');
 
 
-const createComment = ({avatar, name, message }) => {
+const createComment = (comments) => {
+  const {avatar, name, message } = comments;
   const comment = document.createElement('li');
   comment.innerHTML = '<img class="social__picture" src="" alt="" width="35" height="35"><p class="social__text"></p>';
   comment.classList.add('social__picture');
-  comment.querySelector('social__picture').src = avatar;
-  comment.querySelector('social__picture').alt = name;
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
   comment.querySelector('.social__text').textContent = message;
   return comment;
 };
 
 const renderComments = (comments) => {
-  container.document.querySelectorAll('.social__comment').forEach((commentsItem) => commentsItem.remove());
-  container.append(...createComment(comments));
+  container.querySelectorAll('.social__comment').forEach((commentsItem) => commentsItem.remove());
+  container.append(createComment(...comments));
 };
 
 export {renderComments};
