cc60ea6d8de299bd3597b7ba735c869075d9a716
Aleksej <silakovaleksej@icloud.com>
2023-11-17 22:01:38 +0300

    Добавляет подключение для взаимодействия интерфейсов фильтрации.

--- js/gallery/main.js
+++ js/gallery/main.js
@@ -1,9 +1,24 @@
-
+import {renderFilters} from './filters.js';
 import {renderThumbnails} from './thumbnail.js';
 import {renderPopup} from './popup.js';
+import {applyRandomFilter, applyDiscussedFilter} from './data.js';
 
 const setupImageGallery = (imagesData) => {
+  renderFilters();
   renderThumbnails(imagesData);
+  document.addEventListener('filterSelect', (event) => {
+    switch (event.detail) {
+      case 'filter-default':
+        renderThumbnails(imagesData);
+        break;
+      case 'filter-random':
+        renderThumbnails(applyRandomFilter(imagesData));
+        break;
+      case 'filter-discussed':
+        renderThumbnails(applyDiscussedFilter(imagesData));
+        break;
+    }
+  });
 
   document.addEventListener('thumbnailSelect', (event) => {
     renderPopup(event.detail);
