5113ccaf4e782eaaab93f1b8401d6c9f666a0d9d
Aleksej <silakovaleksej@icloud.com>
2023-11-19 14:18:49 +0300

    Добавляет  исправления ошибок.

--- js/gallery/data.js
+++ js/gallery/data.js
@@ -1,11 +1,11 @@
 const applyRandomFilter = (imagesData, itemLimit = 10) => {
   const compare = () => .5 - Math.random();
-  return imagesData.toSorted(compare).slice (0, itemLimit);
+  return [...imagesData].sort(compare).slice(0, itemLimit);
 };
 
 const applyDiscussedFilter = (imagesData) => {
   const compare = (a, b) => b.comments.length - a.comments.length;
-  return imagesData.toSorted(compare);
+  return [...imagesData].sort(compare);
 };
 
 export {applyRandomFilter, applyDiscussedFilter};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {request, throttle} from './utilities.js';
+import {request, debounce} from './utilities.js';
 import {renderStatus} from './status.js';
 import {setupImageGallery} from './gallery/main.js';
 import {setSubmitDisabled, resetForm} from './upload/main.js';
@@ -19,7 +19,7 @@ document.addEventListener('formdata', async (event) => {
 });
 
 try {
-  setupImageGallery(await request(`${baseUrl}data`), throttle);
+  setupImageGallery(await request(`${baseUrl}data`), debounce);
 } catch {
   renderStatus('data-error', {autoHide: 5000});
 }
